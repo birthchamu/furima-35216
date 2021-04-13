89a51f71a7063d93e1ab7482b7ef990675811d52
birthchamu <natural.water.run@gmail.com>
2021-04-13 19:30:14 +0900

    passwordの半角英数字のバリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname         , presence: true
-  validates :password         , presence: true, format: { with: /\A[a-zA-Z0-9]+\z/},  length: { minimum: 6 }
-  validates :first_name       , presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :family_name      , presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :first_name_kana  , presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-  validates :family_name_kana , presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-  validates :date_of_birth    , presence: true
+  validates :nickname           , presence: true
+  validates :password           , format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/}
+  validates :first_name         , presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :family_name        , presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name_kana    , presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :family_name_kana   , presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :date_of_birth      , presence: true
 end
