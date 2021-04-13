910debd0cb1d27b7464f35fcd42b513add2079b8
birthchamu <natural.water.run@gmail.com>
2021-04-13 14:26:57 +0900

    passwordに半角英数字混合のバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname         , presence: true
+  validates :password         , format: { with: /\A[a-zA-Z0-9]+\z/ }
   validates :first_name       , presence: true
   validates :family_name      , presence: true
   validates :first_name_kana  , presence: true
