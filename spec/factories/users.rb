8329a2f8422a0370f586ced5ec3c0e132cb35140
birthchamu <natural.water.run@gmail.com>
2021-04-13 15:34:49 +0900

    userのFactoryBotを編集

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
+    nickname              {'test'}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {'123Test'}
     password_confirmation {password}
+    first_name            {'一郎'}
+    family_name           {'鈴木'}
+    first_name_kana       {'イチロー'}
+    family_name_kana      {'スズキ'}
+    date_of_birth         {'1973-10-22'}
   end
 end
