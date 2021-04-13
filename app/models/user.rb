1b86fa7f91b19b5774e928dec8910e498435bee3
birthchamu <natural.water.run@gmail.com>
2021-04-13 14:30:05 +0900

    ユーザー本名に全角（漢字・ひらがな・カタカナ）のバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname         , presence: true
-  validates :password         , format: { with: /\A[a-zA-Z0-9]+\z/ }
-  validates :first_name       , presence: true
-  validates :family_name      , presence: true
+  validates :password         , format: { with: /\A[a-zA-Z0-9]+\z/}
+  validates :first_name       , presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :family_name      , presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
   validates :first_name_kana  , presence: true
   validates :family_name_kana , presence: true
   validates :date_of_birth    , presence: true
