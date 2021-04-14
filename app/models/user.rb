123d507708eeea95beb53521f01cd957fa72f70a
birthchamu <natural.water.run@gmail.com>
2021-04-14 15:42:41 +0900

    Userモデルのバリデーションを見やすく変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname           , presence: true
-  validates :password           , format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/}
-  validates :first_name         , presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :family_name        , presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :first_name_kana    , presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-  validates :family_name_kana   , presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-  validates :date_of_birth      , presence: true
+  with_option presence: true do
+    validates :nickname
+    validates :password, format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/}
+
+    with_option format: { with: /\A[ぁ-んァ-ン一-龥]/} do
+      validates :first_name 
+      validates :family_name
+    end
+    
+    with_option format: { with: /\A[ァ-ヶー－]+\z/} do
+      validates :first_name_kana
+      validates :family_name_kana 
+    end
+
+    validates :date_of_birth 
+  end
 end
