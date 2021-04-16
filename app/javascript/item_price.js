06037f5524a1244de97499feb5f3fc0846ee2f46
birthchamu <natural.water.run@gmail.com>
2021-04-16 17:07:54 +0900

    不要な記述の削除

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,7 +1,5 @@
 window.addEventListener('load', () => {
 
-
-
 const priceInput = document.getElementById("item-price");
 priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
