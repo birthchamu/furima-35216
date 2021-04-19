a9fa02f37e9fc3a276d727a7c9d56ac243485a46
birthchamu <natural.water.run@gmail.com>
2021-04-19 17:07:26 +0900

    不要な記述の削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :upadate, :destroy]
   before_action :find_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
-  def index
-  end
 
   def new
     @item = Item.new
